ed4160b7d06574b9101555611a672a18a9a868de
joeywang4 <wangjoey4@gmail.com>
2018-03-31 22:04:31 +0000

    Add files via upload

--- chatdata.py
+++ chatdata.py
@@ -1,47 +1,47 @@
 import os
 
-os.remove("chatdata.txt")
+
+
+os.remove("../fatigue-master/chatdata.txt")  
 text = ""
-with open("detect.txt", 'r') as f:
+with open("../fatigue-master/detect.txt", 'r') as f:
    text = f.read()
-
-print(text)
-
-with open("chatdata.txt", 'w') as f:
-   if text[15] != "\"" and text[15] != ",":
+   f.close()
+with open("../fatigue-master/chatdata.txt", 'w') as f:
+  if text[15] != "\"" and text[15] != ",":
       f.write("您眨眼次數為：" + text[14:16] + "次\n")
-      if text[30] == 'y':
+      if text[61] == 'y':
          f.write("您曾姿勢不佳\n")
       else:
          f.write("您姿勢良好\n")
-      if text[46] == 'y':
-         f.write("曾有陌生人拜訪\n")
-      else:
-         f.write("未有不明人士拜訪\n")
-      if text[64] == 'y':
-         f.write("您曾闔上雙眼一段時間\n")
-      else:
-         f.write("您不曾闔上雙眼過久\n")
       if text[77] == 'y':
-         f.write("您曾打了哈欠\n")
-      else:
-         f.write("您不曾打哈欠\n")
-   else:
-      f.write("您眨眼次數為：" + text[14] + "次\n")
-      if text[29] == 'y':
-         f.write("您曾姿勢不佳\n")
-      else:
-         f.write("您姿勢良好\n")
-      if text[45] == 'y':
          f.write("曾有陌生人拜訪\n")
       else:
          f.write("未有不明人士拜訪\n")
-      if text[63] == 'y':
+      if text[32] == 'y':
          f.write("您曾闔上雙眼一段時間\n")
       else:
          f.write("您不曾闔上雙眼過久\n")
-      if text[76] == 'y':
+      if text[45] == 'y':
          f.write("您曾打了哈欠\n")
       else:
          f.write("您不曾打哈欠\n")
-
+  else:
+      print(text[31]+text[44]+text[60]+text[76])      
+      f.write("您眨眼次數為：" + text[14] + "次\n")
+      if text[60] == 'y':
+         f.write("您曾姿勢不佳\n")
+      else:
+         f.write("您姿勢良好\n")
+      if text[76] == 'y':
+         f.write("曾有陌生人拜訪\n")
+      else:
+         f.write("未有不明人士拜訪\n")
+      if text[31] == 'y':
+         f.write("您曾闔上雙眼一段時間\n")
+      else:
+         f.write("您不曾闔上雙眼過久\n")
+      if text[44] == 'y':
+         f.write("您曾打了哈欠\n")
+      else:
+         f.write("您不曾打哈欠\n")
